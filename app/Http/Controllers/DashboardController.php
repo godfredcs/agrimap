f552666f20fc2994fc7402713abeee8648825a4f
Richman Clifford <larriche99@gmail.com>
2017-05-26 10:21:33 -0700

    Implemented drawing of user views graph

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,14 +6,29 @@ use App\Models\User;
 use App\Models\Crop;
 use App\Models\District;
 use Illuminate\Http\Request;
+use App\Services\Statistics;
 
 class DashboardController extends Controller
 {
 	public function index()
     {
+    	$visitors = Statistics::getVisitorsCountThisWeek();
     	$admins = User::all();
     	$districts = District::all();
     	$crops = Crop::all();
         return view('dashboard.dashboard', compact('admins', 'districts', 'crops'));
+    }
+
+    public function getWeekDetails()
+    {
+    	return [
+    	    'Mon' => 10,
+    	    'Tue' => 20,
+    	    'Wed' => 30,
+    	    'Thu' => 40,
+    	    'Fri' => 100,
+    	    'Sat' => 10,
+    	    'Sun' => 5
+    	];
     }
 }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -23,7 +23,7 @@
 
 	        <div class="col-md-3 col-sm-5 col-xs-6 tile_stats_count">
 	            <span class="count_top"><i class="fa fa-search"></i> Views This Week</span>
-	            <div class="count">{{ '230' }}</div>
+	            <div class="count">{{ '215' }}</div>
 	        </div>
 
 	        <div class="col-md-3 col-sm-5 col-xs-6 tile_stats_count">
@@ -31,7 +31,25 @@
 	            <div class="count">{{ count($admins) }}</div>
 	        </div>
 	    </div> 
-	</div>
+
+	    <div class="row">
+	        <div class="col-md-12 col-lg-12 col-sm-6 col-xs-6">
+	            <div class="dashboard_graph">
+	                <div class="row x_title">
+	                    <div class="col-md-12 col-lg-12 col-sm-6">
+	                        <h3>Views This Week</h3>
+	                    </div>
+	                </div>
+
+	                <div class="row">
+	                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	                        <div id="views-this-week" class="demo-placeholder" style="height: 400px"></div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+    </div>
 @endsection
 
 @section('scripts')
